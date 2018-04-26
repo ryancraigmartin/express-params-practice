8f73278c1ca3857553884396f60aa91c85c9579e
Ryan Martin <rmart106@fiu.edu>
2018-04-26 16:59:59 -0400

    Added instructions to README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Instructions
+
+1. Connect to your mongoose database of movies that we used earlier this week (it's called `video`).
+
+2. Make an index.hrb page with all the movies that shows their `title`, `director`, `year`, `rating`.
+
+3. Make a route that looks like this ('/movies/:director') that will display a list of all the movies that were directed by whichever director is typed into the url.
+
+4. make a route that looks like this ('/movies/:year') that displays a list of all movies that were made in the year that is typed into the url
--- app.js
+++ app.js
@@ -4,27 +4,41 @@ const app     = express()
 const hbs     = require('hbs')
 const path = require('path');
 
+mongoose.connect('mongodb://localhost/video')
+  .then(() => {
+    console.log('Connected to Mongo!')
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-  console.log(req)
-})
 
-app.get('/users/:username', function (req, res) {
-  let theUserName = req.params.username;
-  // let theUser = User.find({username: theUserName})   this is how we will query the database
-  let data = {theActualUserName: theUserName }
-  res.render('userpage', data)
-})
+});
 
+app.get('/movies/:director', function (req, res) {
 
-app.get('/search', function (req, res) {
+});
+
+app.get('/movies/:year', function (req, res) {
   res.send(req.query)
-})
+});
 
 
 
+// app.get('/users/:username', function (req, res) {
+//   let theUserName = req.params.username;
+//   // let theUser = User.find({username: theUserName})this is how we will query the database
+//   let data = {theActualUserName: theUserName }
+//   res.render('userpage', data)
+// })
+
+
+// app.get('/search', function (req, res) {
+//   res.send(req.query)
+// })
 
 
 
