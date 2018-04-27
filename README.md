d4fbebb1303e6c7b3f086ab2388e94dea09b3889
Ryan Martin <rmart106@fiu.edu>
2018-04-27 09:59:03 -0400

    Finished the first four iterations.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 2. Make an index.hrb page with all the movies that shows their `title`, `director`, `year`, `rating`.
 
-3. Make a route that looks like this ('/movies/:director') that will display a list of all the movies that were directed by whichever director is typed into the url.
+3. Make a route that looks like this ('/movies/director/:director') that will display a list of all the movies that were directed by whichever director is typed into the url.
 
-4. make a route that looks like this ('/movies/:year') that displays a list of all movies that were made in the year that is typed into the url
+4. Make a route that looks like this ('/movies/year/:year') that displays a list of all movies that were made in the year that is typed into the url
--- app.js
+++ app.js
@@ -1,45 +1,85 @@
-// app.js
 const express = require('express')
 const app     = express()
 const hbs     = require('hbs')
 const path = require('path');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/video')
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+const movieSchema = new Schema({
+  title: {type: String, required: true},
+  director: {type: String, required: true},
+  year: {type: String, required: true},
+  rate: {type: String, required: true},
+  duration: {type: String, required: true},
+  genre:[String]
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+mongoose.connect('mongodb://localhost/video')
+  .then(() => {
+    console.log('Connected to Mongo Database - video')
+  }).catch(err => {
+    console.error('Error connecting to  Mongo Database - video', err)
+  });
+
 app.get('/', function (req, res) {
-
+  Movie.find()
+  .then(movies => {
+    let data = {};
+    data.theList = movies;
+    res.render('index', data);
+  })
+  .catch(theError => {
+    console.log(theError);
+  })
 });
 
-app.get('/movies/:director', function (req, res) {
-
+app.get('/movies/:id', function (req, res) {
+  const theId = req.params.id;
+  Movie.findById(theId)
+  .then(movies => {
+    let data = {};
+    data.theMovie = movies;
+    res.render('movieshow', data);
+  })
+  .catch(theError => {
+    console.log(theError);
+  })
 });
 
-app.get('/movies/:year', function (req, res) {
-  res.send(req.query)
+app.get('/movies/director/:director', function (req, res) {
+  const theDirector = req.params.director;
+  Movie.find({director: theDirector})
+  .then(movies => {
+    let data = {};
+    data.directorList = movies;
+    data.directorName = theDirector;
+    res.render('moviesbydirector', data);
+  })
+  .catch(theError => {
+    console.log(theError);
+  })
 });
 
+app.get('/movies/year/:year', function (req, res) {
+  const theYear = req.params.year;
+  Movie.find({year: theYear})
+  .then(movies => {
+    let data = {};
+    data.yearList = movies;
+    data.selectedYear = theYear;
+    res.render('moviesbyyear', data);
+  })
+  .catch(theError => {
+    console.log(theError);
+  })
+});
 
+app.listen(3000, () => console.log('Server started - localhost:3000'))
 
-// app.get('/users/:username', function (req, res) {
-//   let theUserName = req.params.username;
-//   // let theUser = User.find({username: theUserName})this is how we will query the database
-//   let data = {theActualUserName: theUserName }
-//   res.render('userpage', data)
-// })
-
-
-// app.get('/search', function (req, res) {
-//   res.send(req.query)
-// })
-
-
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//Closes the connection to the video database.
+// mongoose.connection.close();
